aefae4af0bede900b5aceed25fd4092809b60132
Jaime Segura <jseguraweb@gmail.com>
2020-01-21 11:41:49 +0100

    link js and style body and h1

---
+++ index.js
@@ -0,0 +1,15 @@
+// VARIABLES
+
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+
+// TYPOGRAPHY
+
+// body
+body.style.fontFamily = '"Lucida Sans", "Lucida Sans Regular", "Lucida Grande", "Lucida Sans Unicode", Geneva, Verdana, sans-serif';
+body.style.display = 'flex';
+body.style.flexDirection = 'column';
+body.style.alignItems = 'center';
+body.style.justifyContent = 'center';
+// h1
+h1.style.textAlign = 'center';
